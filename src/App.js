767da7085cda5f1864ca8c6a21b19096a278f9dc
abhayvsingh20@gmail.com <abhayvsingh20@gmail.com.com>
2022-12-19 19:25:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         </div>
         <div className='sidebar'>
                 <p>Ad dolore dignissimos asperiores dicta facere optio     quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!</p>
-                <p>Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae tempora quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur at!</p>
+                <p>Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem, ipsam odit quam beatae tempora quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur at!</p>
                 <p>Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto nihil explicabo perferendis quos provident delectus ducimus.</p>
         </div>
         <div className='header'>
